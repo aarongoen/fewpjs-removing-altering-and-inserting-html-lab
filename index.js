c77b9e10f6dc964e84d0c1f662a87aa4e097e1f2
aarongoen <aarongoen2.0@gmail.com>
2021-02-08 15:55:06 -0500

    commit tree dedad431492be91b618d0fae697ae5bd2b0d23a3

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main = document.getElementById('main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Aaron is the champion";
